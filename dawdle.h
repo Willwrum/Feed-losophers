0085315f8d0110c6b8596b3d53b82e9d99880460
Will <165951683+Willwrum@users.noreply.github.com>
2026-04-24 10:53:23 -0700

    Provided files

---
+++ dawdle.h
@@ -0,0 +1,14 @@
+#ifndef DAWDLEH
+#define DAWDLEH
+
+#include <errno.h>
+#include <limits.h>
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void dawdle(void);
+
+#endif
